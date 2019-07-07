91491ac97e88f56d4fdfc9b785b1098681d212ad
Tom Sihap <sihapanya.thomas@gmail.com>
2019-07-07 12:06:42 +0200

    :rocket: Ajout des données existantes dans les pages Edit de student et course

--- public/views/courses/edit.php
+++ public/views/courses/edit.php
@@ -1,18 +1,18 @@
 <?php ob_start(); ?>
 
 <!-- Contenu de la vue -->
-<a href="<?= url('courses') ?>">< Retour au cours</a>
+<a href="<?= url('courses/' . $course->getId() ) ?>">< Retour au cours</a>
 
 <form action="<?= url('courses/edit') ?>" method="post">
 
     <div class="form-group">
         <label for="courseNameForm">Nom du cours</label>
-        <input type="text" name="title" id="courseNameForm" class="form-control">
+        <input type="text" name="title" id="courseNameForm" class="form-control" value="<?= $course->getTitle() ?>">
     </div>
 
     <div class="form-group">
         <label for="courseTeacherForm">Nom du professeur</label>
-        <input type="text" name="teacher" id="courseTeacherForm" class="form-control">
+        <input type="text" name="teacher" id="courseTeacherForm" class="form-control" value="<?= $course->getTeacher() ?>">
     </div>
 
     <button class="btn btn-warning float-right">Mettre à jour le cours</button>
--- public/views/students/edit.php
+++ public/views/students/edit.php
@@ -1,18 +1,18 @@
 <?php ob_start(); ?>
 
 <!-- Contenu de la vue -->
-<a href="<?= url('students') ?>">< Retour à l'étudiant</a>
+<a href="<?= url('students/' .  $student->getId() ) ?>">< Retour à l'étudiant</a>
 
 <form action="<?= url('students/add') ?>" method="post">
 
     <div class="form-group">
         <label for="studentNameForm">Prénom et nom de l'étudiant</label>
-        <input type="text" name="name" id="studentNameForm" class="form-control">
+        <input type="text" name="name" id="studentNameForm" class="form-control" value="<?= $student->getName() ?>">
     </div>
 
     <div class="form-group">
         <label for="studentEmailForm">Adresse e-mail universitaire</label>
-        <input type="text" name="email" id="studentEmailForm" class="form-control">
+        <input type="text" name="email" id="studentEmailForm" class="form-control" value="<?= $student->getEmail() ?>">
     </div>
 
     <button class="btn btn-warning float-right">Mettre à jour l'étudiant</button>
