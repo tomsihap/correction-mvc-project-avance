065a1c8df0fded56ad88c7cec1053d0d2a22da16
Tom Sihap <sihapanya.thomas@gmail.com>
2019-07-07 11:03:50 +0200

    :rocket: Adaptation du fichier config.php pour gérer les liens correctement

--- config/config.php
+++ config/config.php
@@ -7,13 +7,13 @@
 ini_set('allow_url_include', 1);
 
 const DB_HOST = 'localhost';
-const DB_PORT = '3306';
-const DB_NAME = 'blog';
+const DB_PORT = '8889';
+const DB_NAME = 'mvc-harvard';
 const DB_USER = 'root';
-const DB_PWD  = '';
+const DB_PWD  = 'root';
 
 const WEBSITE_TITLE = "";
-const BASE_URL = "http://localhost/examen3";
+const BASE_URL = "http://localhost/cours/mvc-project-correction/";
 
 const CLASSES_SOURCES = [
     'src/controller',
