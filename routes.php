5edb293fbdb1db00b51562c98715e4deed3179b4
Tom Sihap <sihapanya.thomas@gmail.com>
2019-07-07 10:49:41 +0200

    :rocket: Création des routes

---
+++ routes.php
@@ -0,0 +1,34 @@
+<?php
+
+$router = new Router();
+
+$router->get('', 'PagesController@home');
+
+/** Model: Student */
+$router->get('students',            'StudentsController@index');    // Liste des students
+$router->get('students/add',        'StudentsController@add');      // Ajout (affichage formulaire)
+$router->post('students/add',       'StudentsController@save');      // Ajout (traitement formulaire)
+$router->get('students/{id}/edit',  'StudentsController@edit');     // Édition (affichage formulaire)
+$router->post('students/{id}/edit', 'StudentsController@update');   // Édition (traitement formulaire)
+$router->get('students/{id}/delete', 'StudentsController@delete');   // Suppression
+$router->get('students/{id}',       'StudentsController@show');     // Affichage d'un student
+
+/** Model: Course */
+$router->get('courses',            'CoursessController@index');    // Liste des cours
+$router->get('courses/add',        'CoursessController@add');      // Ajout (affichage formulaire)
+$router->post('courses/add',       'CoursessController@save');      // Ajout (traitement formulaire)
+$router->get('courses/{id}/edit',  'CoursessController@edit');     // Édition (affichage formulaire)
+$router->post('courses/{id}/edit', 'CoursessController@update');   // Édition (traitement formulaire)
+$router->get('courses/{id}/delete', 'CoursessController@delete');   // Suppression
+$router->get('courses/{id}',       'CoursessController@show');     // Affichage d'un cours
+
+/** Model: Registration */
+$router->get('registrations',            'RegistrationsController@index');    // Liste des inscriptions
+$router->get('registrations/add',        'RegistrationsController@add');      // Ajout (affichage formulaire)
+$router->post('registrations/add',       'RegistrationsController@save');      // Ajout (traitement formulaire)
+$router->get('registrations/{id}/edit',  'RegistrationsController@edit');     // Édition (affichage formulaire)
+$router->post('registrations/{id}/edit', 'RegistrationsController@update');   // Édition (traitement formulaire)
+$router->get('registrations/{id}/delete', 'RegistrationsController@delete');   // Suppression
+$router->get('registrations/{id}',       'RegistrationsController@show');     // Affichage d'une inscription
+
+$router->run();
